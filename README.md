9d3284d123b97d8ae6b3a8bf40f2de01ab34af04
Nizar Daffa Maulana <izardev@gmail.com>
2024-07-16 12:00:07 +0700

    Day 2

---
+++ README.md
@@ -0,0 +1,1 @@
+# Daily Codewars Javascript 🚀
---
+++ day-2/script.js
@@ -0,0 +1,26 @@
+function narcissistic(value) {
+    const numberString = value.toString()
+    let calculatedNumber = 0
+
+    for (let i = 0; i < numberString.length; i++) {
+        calculatedNumber += (parseInt(numberString[i]) ** numberString.length)
+    }
+
+    console.log(value)
+    console.log(calculatedNumber)
+    console.log(value === calculatedNumber)
+
+    return value === calculatedNumber
+}
+
+const form = document.querySelector('form')
+const number = document.querySelector('#number')
+const result = document.querySelector('#result')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    result.innerHTML = narcissistic(parseInt(form.number.value))
+})
+
+number.innerHTML = form.number.value
