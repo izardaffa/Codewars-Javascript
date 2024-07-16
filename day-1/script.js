64640938d93c02da9918e57a14c04a330d4c2b03
Nizar Daffa Maulana <izardev@gmail.com>
2024-07-17 05:56:49 +0700

    Day 1

---
+++ day-1/script.js
@@ -0,0 +1,14 @@
+function createPhoneNumber(numbers){
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
+const form = document.querySelector('form')
+const result = document.querySelector('#result')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const number = form.number.value
+
+    result.innerText = createPhoneNumber(number)
+})
